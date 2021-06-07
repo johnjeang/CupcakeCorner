7fb21d074714390af777bdb555b47db0e1e11ca6
johnjeang <johnjeang@gmail.com>
2021-06-07 15:08:29 -0500

    Initial Commit

---
+++ CupCakeCorner/CupCakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupCakeCornerApp.swift
+//  CupCakeCorner
+//
+//  Created by John Jeang on 6/7/21.
+//
+
+import SwiftUI
+
+@main
+struct CupCakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
